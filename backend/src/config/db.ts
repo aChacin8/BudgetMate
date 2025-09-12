e71b83d7208e9c817c8eaf0f57cd19d36abc2714
mrMcAko <alechacin08@gmail.com>
2025-09-12 15:28:43 -0600

    feat: Configuracion de la base de datos

---
+++ backend/src/config/db.ts
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+export const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'postgres'
+})
+
