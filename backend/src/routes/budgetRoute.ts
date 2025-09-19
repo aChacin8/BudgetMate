8eca8412be9e6f0f5501ad9cf4fde4569b3a0cb0
mrMcAko <alechacin08@gmail.com>
2025-09-19 12:11:19 -0600

    feat: Agregamos la ruta para manejar la creacion de presupuestos

--- backend/src/routes/budgetRoute.ts
+++ backend/src/routes/budgetRoute.ts
@@ -3,4 +3,5 @@ import { BudgetController } from '../controller/budgetsController';
 
 export const budgetRouter = express.Router();
 
+budgetRouter.post('/budgets', BudgetController.createbudget)
 budgetRouter.get('/budgets', BudgetController.getBudgets)
