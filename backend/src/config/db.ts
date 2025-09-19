cf5fd7ac3994d45293101bfa8082772d021d6d33
mrMcAko <alechacin08@gmail.com>
2025-09-19 09:59:18 -0600

    feat: Agregamos apartado para generar los modelos al hacer npm run

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,12 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 export const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    models: [__dirname + '/../models/**/*'], 
     host: DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
 })
 
