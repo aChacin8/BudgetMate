1c4f60a51a45a24f76673811e3c68c8943c324a1
mrMcAko <alechacin08@gmail.com>
2025-09-22 09:12:13 -0600

    feat: Adding the validations errors to budgets

--- backend/src/routes/budgetRoute.ts
+++ backend/src/routes/budgetRoute.ts
@@ -2,15 +2,24 @@ import express from 'express';
 import { body } from 'express-validator';
 
 import { BudgetController } from '../controllers/BudgetsController';
+import { handleInputErrors } from '../middlewares/hadleInputErrors';
 
 export const budgetRouter = express.Router();
 
 budgetRouter.post('/budgets', 
-    body('name')
-    .notEmpty()
-    .withMessage('Name is required'),
-    BudgetController.createbudget
-    );
+                body('name')
+                    .notEmpty()
+                    .withMessage('Name is required'),
+                body('amount')
+                    .notEmpty()
+                    .withMessage('Amount is required')
+                    .isNumeric()
+                    .withMessage('Amount must be a number')
+                    .custom((value) => value > 0)
+                    .withMessage('Amount must be greater than 0'),
+                handleInputErrors,
+                BudgetController.createbudget
+                );
 budgetRouter.get('/budgets', BudgetController.getBudgets)
 budgetRouter.get('/budgets/:id', BudgetController.getBudgetById)
 budgetRouter.patch('/budgets/:id', BudgetController.updateBudget)
