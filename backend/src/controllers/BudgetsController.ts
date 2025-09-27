59d8a9c032935bfc9172216b7204167bad7b4d64
mrMcAko <alechacin08@gmail.com>
2025-09-26 18:19:14 -0600

    fix: Reordened the budgets controller that validate if a budget exists

--- backend/src/controllers/BudgetsController.ts
+++ backend/src/controllers/BudgetsController.ts
@@ -5,9 +5,6 @@ export class BudgetController {
     static createbudget = async (req: Request, res: Response)=> {
         try {
             const budgets = await Budget.create(req.body)
-            if (!budgets){
-            return res.status(404).json({message: 'Budgets not created'})
-        }
             await budgets.save()
             res.status(201).json(budgets)    
         } catch (error) {
@@ -32,48 +29,16 @@ export class BudgetController {
     }
 
     static getBudgetById = async (req: Request, res: Response) => {
-        try {
-            const { id } = req.params
-            const budget = await Budget.findOne({where: {id}})
-        if (!budget){
-            return res.status(404).json({message: 'Budget not found'})
-        }
-            res.json(budget)
-        } catch (error) {
-            const err = new Error ('Failed to get budget by id')
-            res.status(500).json({message: err.message})
-        }
+        res.json(req.budget)
     }
 
     static updateBudget = async (req: Request, res: Response) => {
-        try {
-            const { id } = req.params
-            const budget = await Budget.findOne({where: {id}})
-            if (budget){
-                await budget.update(req.body)
-            } else {
-                res.status(404).json({message: 'Budget not found, cannot update'})
-            }
-            res.json(budget)
-        } catch (error) {
-            const err = new Error ('Failed to update budget')
-            res.status(500).json({message: err.message})
-        }
+        await req.budget.update(req.body)
+        res.json('Budget updated successfully!')
     }
 
     static deleteBudget = async (req: Request, res: Response) => {
-        try {
-            const { id } = req.params
-            const budget = await Budget.findOne({where: {id}})
-            if (budget){
-                await budget.destroy()
-            } else {
-                res.status(404).json({message: 'Budget not found, cannot delete'})
-            }
-            res.json(budget)
-        } catch (error) {
-            const err = new Error ('Failed to delete budget')
-            res.status(500).json({message: err.message})
-        }
+        await req.budget.destroy()
+        res.json('Budget deleted')
     }
 } 
