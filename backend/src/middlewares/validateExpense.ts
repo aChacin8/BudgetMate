abf430abf97d5a9a8d014cccab30090dbf89b20b
mrMcAko <alechacin08@gmail.com>
2025-09-30 13:17:56 -0600

    fix: Delete unnecesary validation

--- backend/src/middlewares/validateExpense.ts
+++ backend/src/middlewares/validateExpense.ts
@@ -11,19 +11,6 @@ declare global {
     }
 }
 
-export const validateExpense = async (req: Request, res: Response, next: NextFunction) => {
-    await body('name')
-        .notEmpty().withMessage('Name is required')
-        .run(req);
-    await body('amount')
-        .notEmpty().withMessage('Amount is required')
-        .isNumeric().withMessage('Amount must be a number')
-        .custom((value) => value > 0).withMessage('Amount must be greater than 0')
-        .run(req)
-
-    next();
-}
-
 export const validateExpenseById = async (req: Request, res: Response, next: NextFunction) => {
     await param('expenseId')
         .isInt().withMessage('Invalid ID')
