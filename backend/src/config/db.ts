8ea87854314a6aba8d5606d47e0ea01c0790c5d3
aChacin8 <alechacin08@gmail.com>
2025-09-30 14:14:33 -0600

    feat: Adapting db dialect to use .env

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,12 +2,12 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT} = process.env;
 
 export const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     models: [__dirname + '/../models/**/*'], 
     host: DB_HOST,
-    dialect: 'postgres',
+    dialect: DB_DIALECT as any,
     logging: false
 })
 
