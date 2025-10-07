4aa9e462da116341a7d418eea072f32e5ceafccc
mrMcAko <alechacin08@gmail.com>
2025-10-06 18:53:50 -0600

    feat: We've improved the earning model for better management, adding the start and end period for monthly tracking.
    We've also added userId foreings keys.

--- backend/src/models/earning/Earning.ts
+++ backend/src/models/earning/Earning.ts
@@ -1,38 +1,53 @@
-import { Table, Model, Column, DataType, HasMany } from "sequelize-typescript";
+import { Table, Model, Column, DataType, HasMany, ForeignKey, BelongsTo } from "sequelize-typescript";
 import EarningExpense from "./EarningExpense";
 import Budget from "../budget/Budget";
+import User from "../user/User";
+import ExtraEarnings from "./ExtraEarning";
 
 @Table({
-    tableName: 'earnings'
+    tableName: 'earnings',
+    underscored: true
 })
 
 class Earning extends Model {
     @Column({
         type: DataType.DECIMAL(10,2)
     })
-    declare amount: number
+    declare baseAmount: number
 
     @Column({
-        type: DataType.STRING(100)
+        type: DataType.DATE
     })
-    declare monthEarning: string
+    declare periodStart: Date
 
     @Column({
-        type: DataType.INTEGER
+        type: DataType.DATE
     })
-    declare yearAmount: number
+    declare periodEnd: Date
 
-    @HasMany(()=> EarningExpense, {
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-    })
-    declare earningExpenses: EarningExpense[]
+    @ForeignKey(()=> User)
+    declare userId: number
+
+    @BelongsTo(()=> User)
+    declare user: User
 
     @HasMany(()=> Budget, {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
     })
     declare budgets: Budget[]
+    
+    @HasMany(()=> EarningExpense, {
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    })
+    declare earningExpenses: EarningExpense[]
+
+    @HasMany(()=> ExtraEarnings, {
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    })
+    declare extraEarnings: ExtraEarnings[]
 }
 
 export default Earning;
