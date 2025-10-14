6ed762d37e11fd61c19ed24b42f2153ed39f2f07
mrMcAko <alechacin08@gmail.com>
2025-10-13 20:58:38 -0600

    feat: Creating interface to user

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,11 +1,14 @@
+import User from "../models/user/User";
 import BudgetExpense from "../models/budget/BudgetExpense";
 import Budget from "../models/budget/Budget";
 import Expense from "../models/earning/EarningExpense";
 import ExtraEarnings from "../models/earning/ExtraEarning";
 
+
 declare global {
     namespace Express {
         interface Request {
+            user?: User;
             expense?: Expense;
             budget?: Budget;
             budgetExpense?: BudgetExpense;
