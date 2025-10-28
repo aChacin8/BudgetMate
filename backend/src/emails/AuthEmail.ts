72983ad464fad5c90eb0b50ce2edcc027a0ab18a
mrMcAko <alechacin08@gmail.com>
2025-10-28 11:45:00 -0600

    feat: Ending AuthEmail function, ussing decrypted email to send Email to Users

--- backend/src/emails/AuthEmail.ts
+++ backend/src/emails/AuthEmail.ts
@@ -1,10 +1,29 @@
 import { transport } from "../config/nodemailer";
 import { IEmail } from "../interface/email";
+import { CryptoEmail } from "../utils/cryptoEmail";
 
 export class AuthEmail {
-    static sendConfirmEmail = async (user : IEmail) => {
-        const email = await transport.sendMail({
-            from: 'BudgetMate '
-        })
+    static sendConfirmEmail = async (user: IEmail) => {
+        try {            
+            const decryptedEmail = CryptoEmail.decryptEmail(user.email, user.nonce);
+
+            await transport.sendMail({
+                from: `"BudgetMate" <${process.env.EMAIL_USER}>`,
+                to: decryptedEmail, 
+                subject: 'BudgetMate - Confirm your email',
+                html: `
+                    <h1>Hello ${user.firstName} ${user.lastName}</h1>
+                    <p>Thank you for registering with BudgetMate. 
+                        Please confirm your email by clicking the link below:
+                    </p> 
+                    <a href='#'>Confirm Email</a>
+                    <p>Enter the code: <b>${user.token}</b></p>
+                    <p>If you did not create an account, please ignore this email.</p>
+                `
+            });
+        } catch (error) {
+            console.error('Error sending email:', error);
+            throw error;
+        }
     }
 }
