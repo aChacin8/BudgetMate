c6735effb200fbf4dcf1b40e88b41616d22ee875
mrMcAko <alechacin08@gmail.com>
2025-11-03 17:57:34 -0600

    feat: Adding middleware and controller to confirm account

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { AuthController } from '../controllers/auth/AuthController';
 import { handleInputErrors } from '../middlewares/hadleInputErrors';
 import { validateUserById, validateUserExists, validateUserInput } from '../middlewares/validateUsers';
+import { confirmAccountValidation } from '../middlewares/auth/authValidation';
 
 export const authRouter = express.Router();
 
@@ -13,6 +14,12 @@ authRouter.post('',
     validateUserInput,
     handleInputErrors,
     AuthController.createUser
+)
+
+authRouter.post('/confirm-account', 
+    confirmAccountValidation,
+    handleInputErrors,
+    AuthController.confirmAccount
 )
 
 authRouter.post('/login', 
