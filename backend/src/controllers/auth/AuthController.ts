dfe1758352df849f4e7d71e1ded967bd6537808e
mrMcAko <alechacin08@gmail.com>
2025-11-07 11:55:00 -0600

    feat: enhance user creation and login with email encryption and JWT generation

--- backend/src/controllers/auth/AuthController.ts
+++ backend/src/controllers/auth/AuthController.ts
@@ -1,24 +1,23 @@
 import { Request, Response } from "express";
-import jwt from 'jsonwebtoken';
 
 import User from "../../models/user/User";
 import { comparePassword, hashPassword } from "../../utils/hash";
 import { CryptoEmail } from "../../utils/cryptoEmail";
 import { generateToken } from "../../utils/token";
 import { AuthEmail } from "../../emails/AuthEmail";
+import { generateJWT } from "../../utils/jwt";
 
 export class AuthController {
     static createUser = async (req: Request, res: Response) => {
         const { email, password, token, ...rest } = req.body;
-
-        const userExists = await User.findOne({ where: { email } });
-        if (userExists) {
-            return res.status(409).json({ message: 'User already exists' });
-        }
-
+        
         try {
             const { encrypted, nonce } = CryptoEmail.encryptEmail(email);
+            const userExists = await User.findOne({ where: { email: encrypted } });
 
+if (userExists) {
+    return res.status(409).json({ message: 'User already exists' });
+}
             const user = await User.create({
                 ...rest,
                 email: encrypted,
@@ -65,6 +64,7 @@ export class AuthController {
     }
 
     static loginUser = async (req: Request, res: Response) => {
+        console.log('Body recibido:', req.body);
         const { email, password } = req.body
 
         try {
@@ -84,9 +84,17 @@ export class AuthController {
                 return res.status(401).json({ message: 'Invalid password' });
             }
             
-            res.json(user);
+            const jwt = generateJWT(user.id);
+            
+            return res.status(200).json({
+                message: 'Login successful',
+                jwt
+            })
         } catch (error) {
-
+            console.log(email);
+            
+            console.error(error);
+            res.status(500).json({ message: 'Internal server error' });
         }
     }
 }
