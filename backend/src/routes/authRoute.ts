ad95658fb08b5da2aa37578a35065293745d4636
mrMcAko <alechacin08@gmail.com>
2025-11-10 15:33:36 -0600

    feat: Creating forgot and reset password endpoints

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -2,8 +2,9 @@ import express from 'express';
 
 import { AuthController } from '../controllers/auth/AuthController';
 import { handleInputErrors } from '../middlewares/hadleInputErrors';
-import { validateUserById, validateUserExists, validateUserInput } from '../middlewares/validateUsers';
-import { confirmAccountValidation } from '../middlewares/auth/authValidation';
+import { validateUserInput } from '../middlewares/validateUsers';
+import { forgotPasswordValidation } from '../middlewares/auth/authValidation';
+import { confirmAccountValidation, resetTokenValidation } from '../middlewares/auth/tokenValidations';
 import { postLimiter, tokenLimiter } from '../config/limiter';
 
 export const authRouter = express.Router();
@@ -26,5 +27,17 @@ authRouter.post('/login',
     postLimiter,
     handleInputErrors,
     AuthController.loginUser
+)
+
+authRouter.post('/forgot-password',
+    postLimiter,
+    forgotPasswordValidation,
+    handleInputErrors,
+    AuthController.forgotPassword
+)
+
+authRouter.post('/reset-password',
+    tokenLimiter,
+    resetTokenValidation,
 )
 
