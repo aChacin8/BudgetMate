9bf75c077360cff4ece007169e6cc0d5c37d732d
mrMcAko <alechacin08@gmail.com>
2025-11-10 16:49:14 -0600

    fix: Rename AuthController in confirmAccount to TokenController

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { AuthController } from '../controllers/auth/AuthController';
+import { TokenController } from '../controllers/auth/TokenController';
 import { handleInputErrors } from '../middlewares/hadleInputErrors';
 import { validateUserInput } from '../middlewares/validateUsers';
 import { forgotPasswordValidation } from '../middlewares/auth/authValidation';
@@ -20,7 +21,7 @@ authRouter.post('/confirm-account',
     tokenLimiter,
     confirmAccountValidation,
     handleInputErrors,
-    AuthController.confirmAccount
+    TokenController.confirmAccount
 )
 
 authRouter.post('/login', 
@@ -36,8 +37,14 @@ authRouter.post('/forgot-password',
     AuthController.forgotPassword
 )
 
-authRouter.post('/reset-password',
+authRouter.post('/token-password',
     tokenLimiter,
     resetTokenValidation,
+    handleInputErrors,
+    TokenController.tokenResetPassword
+)
+authRouter.post('/reset-password',
+    postLimiter,
+    
 )
 
