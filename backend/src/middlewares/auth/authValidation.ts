1a91dc2a9d8f13c91df34251865807e25f46c657
mrMcAko <alechacin08@gmail.com>
2025-11-10 17:13:23 -0600

    feat: Enhance password validation and add resetPasswordValidation function

--- backend/src/middlewares/auth/authValidation.ts
+++ backend/src/middlewares/auth/authValidation.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 
 export const authValidation = async (req: Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization;
@@ -30,14 +30,36 @@ export const loginValidation = async (req: Request, res: Response, next: NextFun
         .run(req);
     await body('password')
         .notEmpty().withMessage('Password is required')
-        .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+        .isLength({ min: 8 }).withMessage('Password must be at least 6 characters long')
+        .matches(/[0-9]/).withMessage('Password must contain at least one number')
+        .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+        .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+        .matches(/[@$!%*?&]/).withMessage('Password must contain at least one special character')
         .run(req);
+    next();
 }
 
 export const forgotPasswordValidation = async (req: Request, res: Response, next: NextFunction) => {
     await body('email')
         .notEmpty().withMessage('Email is required')
         .isEmail().withMessage('Invalid email format')
+        .run(req);
+    next();
+}
+
+export const resetPasswordValidation = async (req: Request, res: Response, next: NextFunction) => {
+    await param ('token')
+        .notEmpty().withMessage('Token is required')
+        .isNumeric().withMessage('Token must be numeric')
+        .isLength({ min: 6, max: 6 }).withMessage('Token must be 6 digits long')
+        .run(req)
+    await body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 8 }).withMessage('Password must be at least 6 characters long')
+        .matches(/[0-9]/).withMessage('Password must contain at least one number')
+        .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+        .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+        .matches(/[@$!%*?&]/).withMessage('Password must contain at least one special character')
         .run(req);
     next();
 }
