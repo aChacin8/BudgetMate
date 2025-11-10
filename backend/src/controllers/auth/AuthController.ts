3bfe8d6fd28501aeabd9e6d435a821bf22d49498
mrMcAko <alechacin08@gmail.com>
2025-11-10 16:46:01 -0600

    feat: Move confirmAccount method to TokenController for better organization

--- backend/src/controllers/auth/AuthController.ts
+++ backend/src/controllers/auth/AuthController.ts
@@ -47,25 +47,6 @@ export class AuthController {
         }
     }
 
-    static confirmAccount = async (req: Request, res: Response) => {
-        const { token } = req.body;
-
-        try {
-            const user = await User.findOne({ where: { token } });
-            if (!user) {
-                return res.status(404).json({ message: 'Invalid token code' });
-            }
-            user.isConfirmed = true;
-            user.token = null;
-            await user.save();
-
-            return res.status(200).json({ message: 'Account confirmed successfully' });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: 'Internal server error' });
-        }
-    }
-
     static loginUser = async (req: Request, res: Response) => {    
         const { email, password } = req.body
 
