e222c83b80d4a839a5a489c5e58c91ab1d51002a
mrMcAko <alechacin08@gmail.com>
2025-11-10 15:33:00 -0600

    feat: Creatin function to send reset password info to email

--- backend/src/emails/AuthEmail.ts
+++ backend/src/emails/AuthEmail.ts
@@ -26,4 +26,27 @@ export class AuthEmail {
             throw error;
         }
     }
+
+    static sendResetPasswordEmail = async (user: IEmail) => {
+        try {
+            const decryptedEmail = CryptoEmail.decryptEmail(user.email, user.nonce);
+
+            await transport.sendMail({
+                from: `"BudgetMate" <${process.env.EMAIL_USER}>`,
+                to: decryptedEmail, 
+                subject: 'BudgetMate - Confirm your email',
+                html: `
+                    <h1>Hello ${user.firstName} ${user.lastName}</h1>
+                    <p>You have requested to reset your password.
+                        Please confirm your email by clicking the link below:
+                    </p> 
+                    <a href='#'>Reset Password</a>
+                    <p>Enter the code: <b>${user.token}</b></p>
+                    <p>If you did not request a password reset, please ignore this email.</p>`
+            });
+        } catch (error) {
+            console.error('Error sending reset password email:', error);
+            throw error;
+        }
+    }
 }
