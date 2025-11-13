ed510aa5b32b6775444aa0d28d6ca1fa3c3b6931
aChacin8 <alechacin08@gmail.com>
2025-11-13 13:35:05 -0600

    refactor: Remove unused authorization validation logic from authValidation middleware

--- backend/src/middlewares/auth/authValidation.ts
+++ backend/src/middlewares/auth/authValidation.ts
@@ -1,27 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
 import { body, param } from 'express-validator';
-
-export const authValidation = async (req: Request, res: Response, next: NextFunction) => {
-    const bearer = req.headers.authorization;
-    if(!bearer) {
-        const error = new Error ('Authorization token required')
-        return res.status(401).send(error)
-    }
-
-    const [, token] = bearer.split(' ')
-
-    if (!token){
-        const error= new Error ('Token no sent, Not Authorized')
-        return res.status(401).json({ error: error.message })
-    }
-
-    try {
-        const result = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (error) {
-        
-    }
-}
 
 export const loginValidation = async (req: Request, res: Response, next: NextFunction) => {
     await body('email')
