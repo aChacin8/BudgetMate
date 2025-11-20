f5d9d660d1ab3e5a68e17c8bda240e06ec4c8dd3
mrMcAko <alechacin08@gmail.com>
2025-11-20 12:40:03 -0600

    fix: Correct import structure for authentication validation middlewares

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -3,7 +3,8 @@ import express from 'express';
 import { AuthController } from '../controllers/auth/AuthController';
 import { handleInputErrors } from '../middlewares/hadleInputErrors';
 import { validateUserInput } from '../middlewares/validateUsers';
-import { authValidation, checkPasswordValidation, forgotPasswordValidation, resetPasswordValidation, updatePasswordValidation } from '../middlewares/auth/authValidation';
+import { authValidation} from '../middlewares/auth/authValidation';
+import {  checkPasswordValidation, forgotPasswordValidation, resetPasswordValidation, updatePasswordValidation } from '../middlewares/auth/passwordValidation'
 import { confirmAccountValidation, resetTokenValidation } from '../middlewares/auth/tokenValidations';
 import { getLimiter, postLimiter, tokenLimiter } from '../config/limiter';
 
