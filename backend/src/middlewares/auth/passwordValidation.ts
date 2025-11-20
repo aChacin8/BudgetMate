d37393ba7a4bb1696f34e659dd7a0a7f652cf9b0
mrMcAko <alechacin08@gmail.com>
2025-11-20 12:39:57 -0600

    feat: Add password validation middlewares for forgot, reset, update, and check password

---
+++ backend/src/middlewares/auth/passwordValidation.ts
@@ -0,0 +1,59 @@
+import { Request, Response, NextFunction } from 'express';
+import { body, param } from 'express-validator';
+
+export const forgotPasswordValidation = async (req: Request, res: Response, next: NextFunction) => {
+    await body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Invalid email format')
+        .run(req);
+    next();
+}
+
+export const resetPasswordValidation = async (req: Request, res: Response, next: NextFunction) => {
+    await param ('token')
+        .notEmpty().withMessage('Token is required')
+        .isNumeric().withMessage('Token must be numeric')
+        .isLength({ min: 6, max: 6 }).withMessage('Token must be 6 digits long')
+        .run(req)
+    await body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 8 }).withMessage('Password must be at least 6 characters long')
+        .matches(/[0-9]/).withMessage('Password must contain at least one number')
+        .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+        .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+        .matches(/[@$!%*?&]/).withMessage('Password must contain at least one special character')
+        .run(req);
+    next();
+}
+
+export const updatePasswordValidation = async (req: Request, res: Response, next: NextFunction) => {
+    await body('currentPassword')
+        .notEmpty().withMessage('Current Password is required')
+        .isLength({ min: 8 }).withMessage('Current Password must be at least 6 characters long')
+        .matches(/[0-9]/).withMessage('Current Password must contain at least one number')
+        .matches(/[A-Z]/).withMessage('Current Password must contain at least one uppercase letter')
+        .matches(/[a-z]/).withMessage('Current Password must contain at least one lowercase letter')
+        .matches(/[@$!%*?&]/).withMessage('Current Password must contain at least one special character')
+        .run(req);
+    await body('newPassword')
+        .notEmpty().withMessage('New Password is required')
+        .isLength({ min: 8 }).withMessage('New Password must be at least 6 characters long')
+        .matches(/[0-9]/).withMessage('New Password must contain at least one number')
+        .matches(/[A-Z]/).withMessage('New Password must contain at least one uppercase letter')
+        .matches(/[a-z]/).withMessage('New Password must contain at least one lowercase letter')
+        .matches(/[@$!%*?&]/).withMessage('New Password must contain at least one special character')
+        .run(req);
+    next();
+}
+
+export const checkPasswordValidation = async (req: Request, res: Response, next: NextFunction) => {
+    await body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 8 }).withMessage('Password must be at least 6 characters long')
+        .matches(/[0-9]/).withMessage('Password must contain at least one number')
+        .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+        .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+        .matches(/[@$!%*?&]/).withMessage('Password must contain at least one special character')
+        .run(req);
+    next();
+}
