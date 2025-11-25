31c06d8c14d28ef76462420b29640a1b2ff0126a
mrMcAko <alechacin08@gmail.com>
2025-11-24 19:01:15 -0600

    feat: Adding updateEarning function in EarningController

--- backend/src/controllers/earning/EarningController.ts
+++ backend/src/controllers/earning/EarningController.ts
@@ -10,9 +10,9 @@ export class EarningController {
             await earning.save()
             res.status(201).json('Earning created successfully')
         } catch (error) {
-            return res.status(500).json({message: 'Failed to create earning'})
+            return res.status(500).json({ message: 'Failed to create earning' })
         }
-    }
+    };
 
     static getEarning = async (req: Request, res: Response) => {
         try {
@@ -21,12 +21,32 @@ export class EarningController {
                     userId: req.user.id
                 }
             })
-            if(!earning){
-                return  res.status(404).json({message: 'Earnings not found'})
+            if (!earning) {
+                return res.status(404).json({ message: 'Earnings not found' })
             }
-            res.status(200).json({message: 'Earnings retrieved successfully', earning})
+            res.status(200).json({ message: 'Earnings retrieved successfully', earning })
         } catch (error) {
-            return res.status(500).json({message: 'Failed to retrieve earnings'})
+            return res.status(500).json({ message: 'Failed to retrieve earnings' })
         }
-    }
+    };
+
+    static updateEarning = async (req: Request, res: Response) => {
+        const { earningId } = req.params;
+
+        try {
+            const { baseAmount, periodStart, periodEnd } = req.body;
+
+            const earning = await Earning.findByPk(earningId);
+            if (!earning) {
+                return res.status(404).json({ message: "Earning doesn't exist" });
+            }
+
+            await earning.update({ baseAmount, periodStart, periodEnd });
+
+            res.status(200).json({ message: "Earning updated successfully", earning });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    };
 }
