f3b135eb8c7b796c460b59ba75fd16c8b1bfaa0b
mrMcAko <alechacin08@gmail.com>
2025-11-24 18:20:12 -0600

    feat: Adding req,body.userId to middleware to prevent an error
    fix: Using periodStart and periodEnd to date

--- backend/src/middlewares/earnings/earningValidation.ts
+++ backend/src/middlewares/earnings/earningValidation.ts
@@ -3,17 +3,21 @@ import { body } from "express-validator";
 import { Op } from "sequelize"; //To use operator with sequelize
 
 import Earning from "../../models/earning/Earning";
+import User from "../../models/user/User";
 
 export const earningValidation = async (req: Request, res: Response, next: NextFunction) => {
+
     await body('baseAmount')
         .notEmpty().withMessage('Base amount is required')
         .isNumeric().withMessage('Base amount must be a number')
         .custom((value) => value > 0).withMessage('Base amount must be greater than 0')
         .run(req);
+
     await body("periodStart")
         .notEmpty().withMessage("Period start date is required")
         .isISO8601().withMessage("Invalid period start date format")
         .run(req);
+
     await body("periodEnd")
         .notEmpty().withMessage("Period end date is required")
         .isISO8601().withMessage("Invalid period end date format")
@@ -22,9 +26,13 @@ export const earningValidation = async (req: Request, res: Response, next: NextF
     next();
 }
 
+
 export const earningPeriodValidation = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { periodStart, periodEnd } = req.body;
+        const periodStart = new Date(req.body.periodStart);
+        const periodEnd = new Date(req.body.periodEnd);
+
+        req.body.userId = req.user.id;
 
         const existingEarning = await Earning.findOne({
             where: {
@@ -34,28 +42,25 @@ export const earningPeriodValidation = async (req: Request, res: Response, next:
                         periodStart: { [Op.between]: [periodStart, periodEnd] }
                     },
                     {
-                        periodEnd: { [Op.between]: [periodStart, periodEnd]}
+                        periodEnd: { [Op.between]: [periodStart, periodEnd] }
                     },
                     {
                         [Op.and]: [
-                            {
-                                periodStart: {[Op.lt]: periodStart}
-                            },
-                            {
-                                periodEnd: {[Op.gt]: periodEnd}
-                            }
+                            { periodStart: { [Op.lt]: periodStart } },
+                            { periodEnd: { [Op.gt]: periodEnd } }
                         ]
                     }
                 ]
             }
-        })
+        });
 
-        if(existingEarning){
-            return res.status(40).json({ message: 'An earning already exists for this period.'})
+        if (existingEarning) {
+            return res.status(400).json({ message: 'An earning already exists for this period.' });
         }
 
         next();
     } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
+        return res.status(409).json({ message: "Overlapping creation periods" });
     }
 }
+
