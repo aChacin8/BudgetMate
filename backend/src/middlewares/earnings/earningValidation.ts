af9da26f3c11a5bde69713d99f6ef219510921a5
mrMcAko <alechacin08@gmail.com>
2025-12-01 19:38:35 -0600

    refactor: Simplify earning validation by removing unused fields and consolidating checks

--- backend/src/middlewares/earnings/earningValidation.ts
+++ backend/src/middlewares/earnings/earningValidation.ts
@@ -1,9 +1,5 @@
 import { Request, Response, NextFunction } from "express";
 import { body } from "express-validator";
-import { Op } from "sequelize"; //To use operator with sequelize
-
-import Earning from "../../models/earning/Earning";
-import User from "../../models/user/User";
 
 export const earningValidation = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -13,54 +9,21 @@ export const earningValidation = async (req: Request, res: Response, next: NextF
         .custom((value) => value > 0).withMessage('Base amount must be greater than 0')
         .run(req);
 
-    await body("periodStart")
-        .notEmpty().withMessage("Period start date is required")
-        .isISO8601().withMessage("Invalid period start date format")
+    await body("periodType")
+        .notEmpty().withMessage("Period type is required")
+        .isIn(["monthly", "biweekly"]).withMessage("Period type must be either 'monthly' or 'biweekly'")
         .run(req);
 
-    await body("periodEnd")
-        .notEmpty().withMessage("Period end date is required")
-        .isISO8601().withMessage("Invalid period end date format")
+    await body("periodMonth")
+        .notEmpty().withMessage("Period month is required")
+        .isNumeric().withMessage("Period month must be a number")
+        .run(req);
+
+    await body("periodYear")
+        .notEmpty().withMessage("Period year is required")
+        .isNumeric().withMessage("Period year must be a number")
         .run(req);
 
     next();
-}
-
-export const earningPeriodValidation = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const periodStart = new Date(req.body.periodStart);
-        const periodEnd = new Date(req.body.periodEnd);
-
-        req.body.userId = req.user.id;
-
-        const existingEarning = await Earning.findOne({
-            where: {
-                userId: req.user.id,
-                id: { [Op.ne]: req.params.earningId },
-                [Op.or]: [
-                    {
-                        periodStart: { [Op.between]: [periodStart, periodEnd] }
-                    },
-                    {
-                        periodEnd: { [Op.between]: [periodStart, periodEnd] }
-                    },
-                    {
-                        [Op.and]: [
-                            { periodStart: { [Op.lt]: periodStart } },
-                            { periodEnd: { [Op.gt]: periodEnd } }
-                        ]
-                    }
-                ]
-            }
-        });
-
-        if (existingEarning) {
-            return res.status(400).json({ message: 'An earning already exists for this period.' });
-        }
-
-        next();
-    } catch (error) {
-        return res.status(409).json({ message: "Overlapping creation periods" });
-    }
 }
 
