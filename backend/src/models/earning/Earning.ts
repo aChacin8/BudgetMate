51774534f36f8a006bdf7e3e77a1767c66e2d269
mrMcAko <alechacin08@gmail.com>
2025-12-01 19:38:19 -0600

    feat: Reestructuring the earning model, to have a better and easier implematation in periods (Month, year, and Type of period)

--- backend/src/models/earning/Earning.ts
+++ backend/src/models/earning/Earning.ts
@@ -11,42 +11,54 @@ import ExtraEarnings from "./ExtraEarning";
 
 class Earning extends Model {
     @Column({
-        type: DataType.DECIMAL(10,2)
+        type: DataType.DECIMAL(10, 2)
     })
     declare baseAmount: number
 
     @Column({
-        type: DataType.DATE,
-        allowNull: false
+        type: DataType.ENUM("monthly", "biweekly"),
+        allowNull: false,
     })
-    declare periodStart: Date
+    declare periodType: "monthly" | "biweekly";
 
-    @Column({
-        type: DataType.DATE,
-        allowNull: false
+    @Column({ 
+        type: DataType.INTEGER, 
+        allowNull: false 
     })
-    declare periodEnd: Date
+    declare periodMonth: number;
 
-    @ForeignKey(()=> User)
+    @Column({ 
+        type: DataType.INTEGER, 
+        allowNull: false 
+    })
+    declare periodYear: number;
+
+    @Column({ 
+        type: DataType.INTEGER, 
+        allowNull: true 
+    })
+    declare periodNumber: number | null; // 1 o 2 si es quincenal
+
+    @ForeignKey(() => User)
     @Column({ field: 'user_id', type: DataType.INTEGER })
     declare userId: number
 
-    @BelongsTo(()=> User)
+    @BelongsTo(() => User)
     declare user: User
 
-    @HasMany(()=> Budget, {
+    @HasMany(() => Budget, {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
     })
     declare budgets: Budget[]
-    
-    @HasMany(()=> EarningExpense, {
+
+    @HasMany(() => EarningExpense, {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
     })
     declare earningExpenses: EarningExpense[]
 
-    @HasMany(()=> ExtraEarnings, {
+    @HasMany(() => ExtraEarnings, {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
     })
