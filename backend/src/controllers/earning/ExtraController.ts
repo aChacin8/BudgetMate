b44c7dae3cfd46fb0a52ba422d5b2e29a3c7c00b
aChacin8 <alechacin08@gmail.com>
2025-12-03 14:35:15 -0600

    feat: Creating the create and get Extra Ean¿rning Controller

---
+++ backend/src/controllers/earning/ExtraController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import EarningExtras from "../../models/earning/EarningExtra";
+
+export class ExtraController {
+    static createExtra = async (req: Request, res: Response) => {
+        try {
+            const extras = await EarningExtras.create({
+                ...req.body,
+                userId: req.user.id
+            })
+
+            if(!extras){
+                return res.status(400).json({ message: 'Failed to create extra earning' })
+            }
+            extras.save()
+            res.status(201).json({ message: 'Extra Earning created successfully', extras})
+        } catch (error) {
+            const err = new Error ('Internal Server Error')
+            res.status(500).json({message: err.message})
+        }
+    }
+
+    static getExtras = async (req: Request, res: Response) => {
+        try {
+            const extras = await EarningExtras.findAll({
+                order: [['amount', 'DESC']],
+                where: {
+                    userId: req.user.id
+                }
+            })
+            if (!extras){
+            return res.status(404).json({message: 'Extra earnings not found'})
+        }
+            res.status(200).json(extras)
+        } catch (error) {
+            const err = new Error ('Internal Server Error')
+            res.status(500).json({message: err.message})
+        }
+    }
+}
