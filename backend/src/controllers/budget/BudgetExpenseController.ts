b790957504437852a1ebedca0edbc0168ae83921
aChacin8 <alechacin08@gmail.com>
2025-12-03 15:52:23 -0600

    feat: adding getBudgetExpenses to findAll budgets

--- backend/src/controllers/budget/BudgetExpenseController.ts
+++ backend/src/controllers/budget/BudgetExpenseController.ts
@@ -7,10 +7,25 @@ export class BudgetExpenseController {
             const expense = await BudgetExpense.create(req.body)
             expense.budgetId = req.budget.id
             expense.save()
-            res.status(201).json('Expense created successfully')
+            res.status(201).json('Budget Expense created successfully')
         } catch (error) {
             const err = new Error('Failed to create expense')
             res.status(500).json({ message: err.message })
+        }
+    }
+
+    static getBudgetExpenses = async (req: Request, res: Response) => {
+        try {
+            const budgetExpenses = await BudgetExpense.findAll({
+                where: {
+                    userId: req.user.id
+                }
+            })
+            if(!budgetExpenses){
+                return res.status(404).json({ message: 'Budget Expenses not found' })
+            }
+        } catch (error) {
+            return res.status(500).json({ message: 'Internal Server Error' })
         }
     }
 
@@ -21,10 +36,11 @@ export class BudgetExpenseController {
 
     static updateBudgetExpense = async (req: Request, res: Response) => {
         await req.budgetExpense.update(req.body)
-        res.json('Expense updated sucessfully')
+        res.json('Budget Expense updated sucessfully')
     }
 
     static deleteBudgetExpense = async (req: Request, res: Response) => {
         await req.budgetExpense.destroy()
+        res.json('Budget Expense deleted successfully')
     }
 }
