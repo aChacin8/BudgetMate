b26030f0d50484010fcac232f11a251198a499a7
aChacin8 <alechacin08@gmail.com>
2025-12-03 13:58:08 -0600

    feat: Correct import name for extra earnings to match model naming

--- backend/src/interface/index.ts
+++ backend/src/interface/index.ts
@@ -2,7 +2,7 @@ import User from "../models/user/User";
 import BudgetExpense from "../models/budget/BudgetExpense";
 import Budget from "../models/budget/Budget";
 import Expense from "../models/earning/EarningExpense";
-import ExtraEarnings from "../models/earning/ExtraEarning";
+import EarningExtras from "../models/earning/EarningExtra";
 import Earning from "../models/earning/Earning";
 
 
@@ -14,7 +14,7 @@ declare global {
             expense?: Expense;
             budget?: Budget;
             budgetExpense?: BudgetExpense;
-            extraEarning?: ExtraEarnings;
+            extraEarning?: EarningExtras;
         }
     }
 }
